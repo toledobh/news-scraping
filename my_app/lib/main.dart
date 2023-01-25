82baa7c6b2668c549ada3dd7c5079262c65e0f87
DanielSaAmaro <121978184+DanielSaAmaro@users.noreply.github.com>
2023-01-25 15:04:26 -0700

    Update main.dart
    removed unused counter variable removed test comments

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -27,16 +27,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // Here we take the value from the MyHomePage object that was created by
       // the App.build method, and use it to set our appbar title.
-      //  this is test
-      // Silly comment
-      // Sillier comment
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -58,7 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ]),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
