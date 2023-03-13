ac759a95bcb648e72d400531b67398e16b54550b
Maho <122491961+MahoGr@users.noreply.github.com>
2023-03-13 15:47:01 -0600

    working swiping page

--- my_app/lib/red_screen.dart
+++ my_app/lib/red_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class KeepTryingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /*
     return Container(
       color: Colors.red,
     );
+    */
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test'),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 203, 66, 66),
+        ),
+      );
   }
 }
